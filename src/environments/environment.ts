de775633a578dd09a17754f3671bd6871de9375b
Matiro00 <matias301020000@gmail.com>
2022-11-07 00:04:11 -0300

    fix: Arreglando url de amazon y conf de la app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiUrl: 'http://ec2-18-228-43-203.sa-east-1.compute.amazonaws.com:5555',
   socketUrl: 'ws://ec2-18-228-43-203.sa-east-1.compute.amazonaws.com:5555',
   allowedDomains: ['ec2-18-228-43-203.sa-east-1.compute.amazonaws.com:5555']
-};
+} as any;
 
 /*
  * For easier debugging in development mode, you can import the following file
